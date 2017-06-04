f55ec497d8c09a35568000ee74affaa28739e689
Rustam Musin <PloadyFree@gmail.com>
2017-06-05 01:50:03 +0400

    8. Добавлена домашняя страница

--- src/main/java/istu/bacs/web/MainController.java
+++ src/main/java/istu/bacs/web/MainController.java
@@ -1,13 +1,14 @@
 package istu.bacs.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "Hello, Spring!";
+		return "index";
 	}
+	
 }
