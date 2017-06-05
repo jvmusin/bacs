5fc6347f35ee755a7ee4140a5b95143ad4bff8d9
Rustam Musin <PloadyFree@gmail.com>
2017-06-05 13:38:28 +0400

    10. Добавлена страница с задачами контеста

--- src/main/java/istu/bacs/web/ContestController.java
+++ src/main/java/istu/bacs/web/ContestController.java
@@ -3,12 +3,14 @@ package istu.bacs.web;
 import istu.bacs.service.ContestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContestController {
 	
 	private static final String VIEWS_CONTEST_LIST = "contests/contest-list";
+	private static final String VIEWS_CONTEST_PROBLEMS = "contests/contest-problems";
 	
 	private final ContestService contestService;
 	
@@ -20,6 +22,12 @@ public class ContestController {
 	public String getAllContests(Model model) {
 		model.addAttribute("contests", contestService.findAll());
 		return VIEWS_CONTEST_LIST;
+	}
+	
+	@RequestMapping("/contest/{contestId}")
+	public String getContest(Model model, @PathVariable Integer contestId) {
+		model.addAttribute("contest", contestService.findById(contestId));
+		return VIEWS_CONTEST_PROBLEMS;
 	}
 	
 }
