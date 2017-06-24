010cafa9467612dc952b822c9015a34cc0d1c1fb
Rustam Musin <PloadyFree@gmail.com>
2017-06-24 16:33:23 +0400

    16. Добавлена возможность отправки решения

--- src/main/java/istu/bacs/web/ContestController.java
+++ src/main/java/istu/bacs/web/ContestController.java
@@ -1,11 +1,19 @@
 package istu.bacs.web;
 
+import istu.bacs.model.Submission;
+import istu.bacs.model.User;
+import istu.bacs.model.type.Language;
+import istu.bacs.model.type.Verdict;
 import istu.bacs.service.ContestService;
 import istu.bacs.service.SubmissionService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Controller
 public class ContestController {
@@ -13,6 +21,7 @@ public class ContestController {
 	private static final String VIEWS_CONTEST_LIST = "contests/contest-list";
 	private static final String VIEWS_CONTEST_PROBLEMS = "contests/contest-problems";
 	private static final String VIEWS_SUBMISSION_LIST = "contests/submissions/submission-list";
+	private static final String VIEWS_SUBMIT_PAGE = "contests/submissions/submit-solution";
 	
 	private final ContestService contestService;
 	private final SubmissionService submissionService;
@@ -39,6 +48,31 @@ public class ContestController {
 		model.addAttribute("submissions", submissionService.findAllByContestId(contestId));
 		model.addAttribute("contestName", contestService.findById(contestId).getContestName());
 		return VIEWS_SUBMISSION_LIST;
+	}
+	
+	@GetMapping("/contest/{contestId}/submit")
+	public String loadSubmissionForm(Model model, @PathVariable int contestId) {
+		model.addAttribute("submission", new Submission());
+		model.addAttribute("languages", Language.values());
+		model.addAttribute("problems", contestService.findById(contestId).getProblems());
+		return VIEWS_SUBMIT_PAGE;
+	}
+	
+	@PostMapping("/contest/{contestId}/submit")
+	public String submit(@ModelAttribute Submission submission,
+						 @PathVariable Integer contestId,
+						 @RequestParam MultipartFile file,
+						 @AuthenticationPrincipal User user) throws IOException {
+		if (!file.getOriginalFilename().isEmpty())
+			submission.setSolution(new String(file.getBytes()));
+		submission.setAuthor(user);
+		submission.setContest(contestService.findById(contestId));
+		submission.setCreationTime(LocalDateTime.now());
+		submission.setVerdict(Verdict.Accepted);
+		submission.setTimeConsumedMillis(1.23 * 1000);
+		submission.setMemoryConsumedBytes(21.4 * 1024 * 1024);
+		submissionService.save(submission);
+		return "redirect:/contest/{contestId}/submissions";
 	}
 	
 }
