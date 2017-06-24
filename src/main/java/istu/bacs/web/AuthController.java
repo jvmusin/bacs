9dea2d3692c6cf02cc4c6ba2aef883f4e0757377
Rustam Musin <PloadyFree@gmail.com>
2017-06-24 16:50:36 +0400

    18. Добавлена проверка на валидность username/password

--- src/main/java/istu/bacs/web/AuthController.java
+++ src/main/java/istu/bacs/web/AuthController.java
@@ -2,8 +2,10 @@ package istu.bacs.web;
 
 import istu.bacs.model.User;
 import istu.bacs.service.UserService;
+import istu.bacs.service.UsernameAlreadyInUseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,8 +35,22 @@ public class AuthController {
 	}
 	
 	@PostMapping("/register")
-	public String register(@ModelAttribute User user) {
-		userService.register(user);
+	public String register(@ModelAttribute User user, Errors errors) {
+		int usernameLen = user.getUsername().length();
+		int passwordLen = user.getPassword().length();
+		if (!(6 <= usernameLen && usernameLen <= 20))
+			errors.rejectValue("username", "wronglen", "Username length should be inside [6, 20]");
+		if (!(6 <= passwordLen && passwordLen <= 20))
+			errors.rejectValue("password", "wronglen", "Password length should be inside [6, 20]");
+		if (errors.hasErrors())
+			return VIEWS_REGISTER_FORM;
+		
+		try {
+			userService.register(user);
+		} catch (UsernameAlreadyInUseException ignored) {
+			errors.rejectValue("username", "taken", "This username is already taken");
+			return VIEWS_REGISTER_FORM;
+		}
 		return "redirect:/login";
 	}
 }
