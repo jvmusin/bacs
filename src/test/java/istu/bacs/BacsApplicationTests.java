b7146fcbe882fdaaf06a2de329c86b1861d4d728
Rustam Musin <PloadyFree@gmail.com>
2017-11-29 23:41:43 +0400

    Comment tests

--- src/test/java/istu/bacs/BacsApplicationTests.java
+++ src/test/java/istu/bacs/BacsApplicationTests.java
@@ -1,16 +1,16 @@
-package istu.bacs;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BacsApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package istu.bacs;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class BacsApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
