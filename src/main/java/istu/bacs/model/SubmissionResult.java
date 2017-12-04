22fe7a8d9852258dbe715ea9f9c2ef71c5857238
Rustam Musin <PloadyFree@gmail.com>
2017-12-05 00:09:49 +0400

    Let SubmissionResult contain null time/memory and add testsPassed

--- src/main/java/istu/bacs/model/SubmissionResult.java
+++ src/main/java/istu/bacs/model/SubmissionResult.java
@@ -1,21 +1,20 @@
 package istu.bacs.model;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
-import javax.xml.transform.OutputKeys;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class SubmissionResult {
     private boolean built;
     private String buildInfo;
 
     private Verdict verdict;
-    private Integer firstFailedTest;
-    private int timeUsedMillis;
-    private int memoryUsedBytes;
+    private Integer testsPassed;
+    private Integer timeUsedMillis;
+    private Integer memoryUsedBytes;
 
     private List<TestGroupResult> testGroupResults;
 }
