cf9bac6a7a8ccdd900ce6db61b655dce41d56abd
Rustam Musin <PloadyFree@gmail.com>
2017-12-05 21:41:24 +0400

    Add DtoUtils to work with datetime formats

---
+++ src/main/java/istu/bacs/web/dto/DtoUtils.java
@@ -0,0 +1,17 @@
+package istu.bacs.web.dto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DtoUtils {
+    public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm";
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+
+    public static String format(LocalDateTime time) {
+        return formatter.format(time);
+    }
+
+    public static LocalDateTime parse(String time) {
+        return (LocalDateTime) formatter.parse(time);
+    }
+}
