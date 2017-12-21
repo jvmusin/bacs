d1e503aeb538cc207595199d4c9c011c28da68ab
Rustam Musin <PloadyFree@gmail.com>
2017-12-21 19:52:40 +0400

    Reformat schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,32 +1,32 @@
-CREATE TABLE IF NOT EXISTS user(
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS user (
+  user_id  INT PRIMARY KEY AUTO_INCREMENT,
 
   username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS contest(
-  contest_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS contest (
+  contest_id   INT PRIMARY KEY AUTO_INCREMENT,
 
   contest_name VARCHAR(255) NOT NULL,
 
-  start_time DATETIME NULL,
-  finish_time DATETIME NULL,
+  start_time   DATETIME     NOT NULL,
+  finish_time  DATETIME     NOT NULL,
 
-  problems VARCHAR(255) NOT NULL
+  problems     VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS submission(
-  submission_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS submission (
+  submission_id          INT PRIMARY KEY AUTO_INCREMENT,
 
-  author_id INT NOT NULL,
-  contest_id INT NOT NULL,
-  problem_id VARCHAR(255) NOT NULL,
+  author_id              INT          NOT NULL,
+  contest_id             INT          NOT NULL,
+  problem_id             VARCHAR(255) NOT NULL,
 
-  pretests_only BOOLEAN NOT NULL,
-  creation_time DATETIME NOT NULL,
-  language INT NOT NULL,
-  solution BLOB NOT NULL,
+  pretests_only          BOOLEAN      NOT NULL,
+  creation_time          DATETIME     NOT NULL,
+  language               INT          NOT NULL,
+  solution               BLOB         NOT NULL,
 
   external_submission_id VARCHAR(255) NOT NULL
 );
