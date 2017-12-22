614dcd929d993c9a4d96db922e7755b7299cde4b
Rustam Musin <PloadyFree@gmail.com>
2017-12-22 17:37:16 +0400

    Enable JWT auth

---
+++ src/main/java/istu/bacs/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package istu.bacs.security;
+
+public class SecurityConstants {
+    public static final String SECRET = "SecretKeyToGenJWTs";   //todo: simple, but enough
+    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/sign-up";
+}
