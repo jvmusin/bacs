375f91d866af816d47fb2bd50c64bd2f7fb40680
Rustam Musin <PloadyFree@gmail.com>
2018-01-13 21:55:51 +0400

    Return 403 status when user registration failed

--- src/main/java/istu/bacs/user/UserController.java
+++ src/main/java/istu/bacs/user/UserController.java
@@ -1,8 +1,10 @@
 package istu.bacs.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @RestController
 public class UserController {
@@ -14,12 +16,18 @@ public class UserController {
     }
 
     @PostMapping("/sign-up")
-    public void signUp(@RequestBody NewUserDto user) {
+    public ResponseEntity<?> signUp(@RequestBody NewUserDto user) {
         User u = new User();
         u.setUsername(user.getUsername());
         u.setPassword(user.getPassword());
-        u.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_USER"));
-        userService.signUp(u);
+        u.setAuthorities(createAuthorityList("ROLE_USER"));
+
+        try {
+            userService.signUp(u);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(403).body(e.getMessage());
+        }
     }
 
     @GetMapping("/user/{userId}")
