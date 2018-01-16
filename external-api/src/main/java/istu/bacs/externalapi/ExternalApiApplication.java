98f4bf67d8eedbff7254e0411d89ed1035f0a9ab
Rustam Musin <PloadyFree@gmail.com>
2018-01-17 01:20:32 +0400

    Fix build failures

---
+++ external-api/src/main/java/istu/bacs/externalapi/ExternalApiApplication.java
@@ -0,0 +1,11 @@
+package istu.bacs.externalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApiApplication.class, args);
+    }
+}
