6c14e80d5669ef25f556ea9a6c06cd7bdd32cf48
Rustam Musin <PloadyFree@gmail.com>
2018-01-19 14:36:19 +0400

    Added sybon fake base

---
+++ external-api-sybon-fake/src/main/java/istu/bacs/externalapi/sybonfake/SybonFakeConfiguration.java
@@ -0,0 +1,9 @@
+package istu.bacs.externalapi.sybonfake;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("sybon-fake")
+public class SybonFakeConfiguration {
+}
