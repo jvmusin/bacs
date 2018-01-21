9b19fdb238a7fddddb2bac507221c4199ba90a34
Rustam Musin <PloadyFree@gmail.com>
2018-01-21 13:32:57 +0400

    Run submission-submitter-and-checker in parallel threads

--- submission-submitter-and-checker/src/main/java/istu/bacs/submissionsubmitterandchecker/SubmissionSubmitterAndCheckerApplication.java
+++ submission-submitter-and-checker/src/main/java/istu/bacs/submissionsubmitterandchecker/SubmissionSubmitterAndCheckerApplication.java
@@ -2,12 +2,22 @@ package istu.bacs.submissionsubmitterandchecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableScheduling
 public class SubmissionSubmitterAndCheckerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubmissionSubmitterAndCheckerApplication.class, args);
+    }
+
+    @SuppressWarnings("ContextJavaBeanUnresolvedMethodsInspection")
+    @Bean(destroyMethod = "shutdown")
+    public Executor taskScheduler() {
+        return Executors.newScheduledThreadPool(2);
     }
 }
