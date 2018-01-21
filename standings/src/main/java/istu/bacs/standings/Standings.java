4cbeeece4024575c4e25432991c957b0ea389f60
Rustam Musin <PloadyFree@gmail.com>
2018-01-21 20:24:18 +0400

    Move unacceptableVerdicts to Standings class

--- standings/src/main/java/istu/bacs/standings/Standings.java
+++ standings/src/main/java/istu/bacs/standings/Standings.java
@@ -1,18 +1,19 @@
 package istu.bacs.standings;
 
 import istu.bacs.db.submission.Submission;
+import istu.bacs.db.submission.Verdict;
 import istu.bacs.db.user.User;
 import lombok.extern.java.Log;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import static istu.bacs.db.submission.Verdict.*;
 import static java.util.Comparator.comparingInt;
 
 @Log
 public class Standings {
+
+    private static final Set<Verdict> unacceptableVerdicts = EnumSet.of(NOT_SUBMITTED, PENDING, COMPILE_ERROR);
 
     private final Map<User, ContestantRow> byUser = new HashMap<>();
     private final List<ContestantRow> rows = new ArrayList<>();
@@ -24,6 +25,10 @@ public class Standings {
     public void update(Submission submission) {
         synchronized (this) {
             log.info("Updating standings for submission " + submission.getSubmissionId());
+
+            if (unacceptableVerdicts.contains(submission.getVerdict()))
+                return;
+
             byUser.computeIfAbsent(submission.getAuthor(), author -> {
                 ContestantRow row = new ContestantRow(author, submission.getContest().getProblems());
                 rows.add(row);
--- standings/src/main/java/istu/bacs/standings/service/StandingsServiceImpl.java
+++ standings/src/main/java/istu/bacs/standings/service/StandingsServiceImpl.java
@@ -1,7 +1,6 @@
 package istu.bacs.standings.service;
 
 import istu.bacs.db.submission.Submission;
-import istu.bacs.db.submission.Verdict;
 import istu.bacs.standings.Standings;
 import istu.bacs.standings.db.SubmissionService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,17 +8,12 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 import javax.transaction.Transactional;
-import java.util.EnumSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static istu.bacs.db.submission.Verdict.*;
 import static istu.bacs.rabbit.QueueNames.CHECKED_SUBMISSIONS;
 
 public class StandingsServiceImpl implements StandingsService, ApplicationListener<ContextRefreshedEvent> {
-
-    private static final Set<Verdict> unacceptableVerdicts = EnumSet.of(NOT_SUBMITTED, PENDING, COMPILE_ERROR);
 
     private final SubmissionService submissionService;
     private final Map<Integer, Standings> standingsByContestId = new ConcurrentHashMap<>();
@@ -30,8 +24,8 @@ public class StandingsServiceImpl implements StandingsService, ApplicationListen
 
     @Override
     public void update(Submission submission) {
-        if (!unacceptableVerdicts.contains(submission.getVerdict()))
-            getStandings(submission.getContest().getContestId()).update(submission);
+        Standings standings = getStandings(submission.getContest().getContestId());
+        standings.update(submission);
     }
 
     @RabbitListener(queues = CHECKED_SUBMISSIONS)
