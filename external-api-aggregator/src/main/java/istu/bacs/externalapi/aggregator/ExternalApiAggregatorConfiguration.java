ccd895d219d92f10f58040cf4df4d8fead8937e7
Rustam Musin <PloadyFree@gmail.com>
2018-01-21 16:16:26 +0400

    Make only one ExternalApiAggregator bean

--- external-api-aggregator/src/main/java/istu/bacs/externalapi/aggregator/ExternalApiAggregatorConfiguration.java
+++ external-api-aggregator/src/main/java/istu/bacs/externalapi/aggregator/ExternalApiAggregatorConfiguration.java
@@ -1,23 +1,28 @@
 package istu.bacs.externalapi.aggregator;
 
 import istu.bacs.externalapi.ExternalApi;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
-public class ExternalApiAggregatorConfiguration {
+public class ExternalApiAggregatorConfiguration implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Bean
-    @ConditionalOnBean(ExternalApi.class)
-    public ExternalApiAggregator externalApiAggregator(ExternalApi[] externalApis) {
+    public ExternalApiAggregator externalApiAggregator() {
+        Map<String, ExternalApi> beans = applicationContext.getBeansOfType(ExternalApi.class);
+        ExternalApi[] externalApis = beans.values().toArray(new ExternalApi[0]);
         return new ExternalApiAggregatorImpl(externalApis);
     }
 
-    @Bean
-    @ConditionalOnMissingBean(ExternalApi.class)
-    public ExternalApiAggregator externalApiAggregatorEmpty() {
-        return new ExternalApiAggregatorImpl(new ExternalApi[0]);
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
