55883784f4d1732f52ee2f445cc49aaa3d76981a
Rustam Musin <PloadyFree@gmail.com>
2018-01-24 01:07:35 +0400

    Fix 500 error on /contests/{contestId}/submissions/my

--- db/src/main/java/istu/bacs/db/submission/SubmissionRepository.java
+++ db/src/main/java/istu/bacs/db/submission/SubmissionRepository.java
@@ -4,10 +4,14 @@ import istu.bacs.db.contest.ContestProblem;
 import istu.bacs.db.user.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Integer> {
     List<Submission> findAllByContestProblem(List<ContestProblem> problems, Pageable pageable);
-    List<Submission> findAllByAuthorAndContestProblem(User author, List<ContestProblem> problems, Pageable pageable);
+
+    @Query("SELECT s FROM Submission s WHERE s.author = :author AND s.contestProblem IN (:problems)")
+    List<Submission> findAllByAuthorAndContestProblem(@Param("author") User author, @Param("problems") List<ContestProblem> problems, Pageable pageable);
 }
