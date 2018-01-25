e3f5a69e29836d48adc5c38ac94710bc4a58faa2
Rustam Musin <PloadyFree@gmail.com>
2018-01-25 16:59:55 +0400

    Make combined application run without port

---
+++ external-api-sybon/src/main/java/istu/bacs/externalapi/sybon/SybonApiApplication.java
@@ -0,0 +1,11 @@
+package istu.bacs.externalapi.sybon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SybonApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SybonApiApplication.class, args);
+    }
+}
