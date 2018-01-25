9af13a4959f1e35e9caf86c3e26cfb8417742cca
Rustam Musin <PloadyFree@gmail.com>
2018-01-25 15:55:02 +0400

    Make connection to db without 'db' profile active

---
+++ external-api-aggregator/src/main/java/istu/bacs/externalapi/aggregator/ExternalApiAggregatorApplication.java
@@ -0,0 +1,11 @@
+package istu.bacs.externalapi.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiAggregatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApiAggregatorApplication.class, args);
+    }
+}
