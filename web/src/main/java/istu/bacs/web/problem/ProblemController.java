3141bb45195ef35ccf893955424333b11b5adfee
Rustam Musin <PloadyFree@gmail.com>
2018-02-11 17:17:36 +0400

    Add /contests?contestId={cId}&problemId={problemId}

--- web/src/main/java/istu/bacs/web/problem/ProblemController.java
+++ web/src/main/java/istu/bacs/web/problem/ProblemController.java
@@ -1,10 +1,13 @@
 package istu.bacs.web.problem;
 
+import istu.bacs.db.problem.Problem;
 import istu.bacs.externalapi.ExternalApi;
+import istu.bacs.web.contest.ContestService;
 import istu.bacs.web.model.problem.ArchiveProblem;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -14,14 +17,23 @@ import static java.util.stream.Collectors.toList;
 @AllArgsConstructor
 public class ProblemController {
 
+    private final ContestService contestService;
     private final ProblemService problemService;
     private final ExternalApi externalApi;
 
     @GetMapping
-    public List<ArchiveProblem> getAllProblems(@RequestParam(name = "external", required = false) String external) {
+    public List<ArchiveProblem> getAllProblems(@RequestParam(name = "external", required = false) String external,
+                                               @RequestParam(name = "contestId", required = false) Integer contestId,
+                                               @RequestParam(name = "problemIndex", required = false) String problemIndex) {
         if (external != null) {
             problemService.saveAll(externalApi.getAllProblems());
         }
+
+        if (contestId != null && problemIndex != null) {
+            Problem problem = contestService.findById(contestId).getProblem(problemIndex).getProblem();
+            return Collections.singletonList(ArchiveProblem.fromDb(problem));
+        }
+
         return problemService.findAllProblems().stream()
                 .map(ArchiveProblem::fromDb)
                 .collect(toList());
