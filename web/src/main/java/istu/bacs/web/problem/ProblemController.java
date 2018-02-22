0a0f6b9bae5d81548edcaacf5af21396108096fc
Rustam Musin <PloadyFree@gmail.com>
2018-02-22 18:51:40 +0400

    Add an option to get contest problems using /problems

--- web/src/main/java/istu/bacs/web/problem/ProblemController.java
+++ web/src/main/java/istu/bacs/web/problem/ProblemController.java
@@ -1,6 +1,8 @@
 package istu.bacs.web.problem;
 
+import istu.bacs.db.problem.Problem;
 import istu.bacs.externalapi.ExternalApi;
+import istu.bacs.web.contest.ContestService;
 import istu.bacs.web.model.problem.ArchiveProblem;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -8,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -18,16 +21,25 @@ import static java.util.stream.Collectors.toList;
 public class ProblemController {
 
     private final ProblemService problemService;
+    private final ContestService contestService;
     private final ExternalApi externalApi;
 
     @GetMapping
     public List<ArchiveProblem> getAllProblems(
             @RequestParam(name = "external", required = false) String external,
+            @RequestParam(name = "contestId", required = false) Integer contestId,
+            @RequestParam(name = "problemIndex", required = false) String problemIndex,
             @RequestParam(required = false, defaultValue = "0") int pageIndex,
             @RequestParam(required = false, defaultValue = "50") int pageSize) {
 
-        if (external != null)
+        if (external != null) {
             problemService.saveAll(externalApi.getAllProblems());
+        }
+
+        if (contestId != null && problemIndex != null) {
+            Problem problem = contestService.findById(contestId).getProblem(problemIndex).getProblem();
+            return Collections.singletonList(ArchiveProblem.fromDb(problem));
+        }
 
         Pageable pageable = PageRequest.of(pageIndex, pageSize, Sort.by("contestId").descending());
 
