c6b024260d3fb0cafa4ef40c1ec0ec370785ce12
Rustam Musin <PloadyFree@gmail.com>
2018-03-08 19:43:39 +0400

    Use postgres profile to connect to database

--- db/src/main/java/istu/bacs/db/DbConfiguration.java
+++ db/src/main/java/istu/bacs/db/DbConfiguration.java
@@ -2,12 +2,10 @@ package istu.bacs.db;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@PropertySource("classpath:application-db.properties")
 @EntityScan
 @EnableJpaRepositories
 @EnableTransactionManagement
